05979b4a0668aea326d4542ded334e876984616a
Claudia <clau050994@gmail.com>
2023-12-20 11:48:29 -0500

    Initial commit

--- game/src/constants.js
+++ game/src/constants.js
@@ -1,4 +1,6 @@
 const gravity = new BABYLON.Vector2(0, -9.8);
 const flightForce = 5; // The amount of force applied when the "fly" button is pressed
 
-const greetings = ["It's a bird!", "It's a plane!", "1000% more Cube!", "A Whole New Dimension!"];
+const greetings = ["It's a bird!", "It's a plane!", "1000% more Cube!", "A Whole New Dimension!,", "Welcome to the adventure!"
+,"Ready to soar!","Prepare for liftoff!","Time to defy gravity!","Incredible heights await!","Let's reach for the stars!","Fly like a pro!",
+"Soaring to new horizons!",];

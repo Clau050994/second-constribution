e3e600d00879db9da0f66518c3ee8541f828bd6d
Clau050994 <111773719+Clau050994@users.noreply.github.com>
2023-12-21 16:45:31 +0000

    Deploying to gh-pages from @ Clau050994/second-constribution@e4176be26ba9ba879597030322a9bf29b1edcac6 🚀

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+const gravity = new BABYLON.Vector2(0, -9.8);
+const flightForce = 5; // The amount of force applied when the "fly" button is pressed
+
+const greetings = ["It's a bird!", 
+"It's a plane!", 
+"1000% more Cube!", 
+"A Whole New Dimension!",
+"Adventure Awaits!",
+"Dive into Excitement!",
+"Unlock the Mysteries!",
+"Embrace the Unknown!",
+"Journey to New Realms!",
+"Discover Hidden Wonders!",
+"Unleash Your Imagination!",
+"Beyond the Horizon!",
+"Epic Adventures Ahead!",
+"Challenge the Impossible!",
+"Ready for the Quest!",
+"Into the Future!",
+"The Sky's the Limit!",
+"Experience the Magic!",
+"Welcome to the Next Level!"];
+
